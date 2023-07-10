a75b2a42d84eae36b8e5af5ef628be686f71e5ad
summermong <2514o_o@naver.com>
2023-07-10 14:57:29 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testid="counter"> {count} </h3>
+        <button
+          data-testid="minus-button"
+          onClick={() => setCount((count) => count - 1)}
+          disabled={disabled}
+        >
+          -
+        </button>
+        <button
+          data-testid="plus-button"
+          onClick={() => setCount((count) => count + 1)}
+          disabled={disabled}
+        >
+          +
+        </button>
+        <div>
+          <button
+            data-testid="on/off button"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setDisabled((prev) => !prev)}
+          >
+            on/off button
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
